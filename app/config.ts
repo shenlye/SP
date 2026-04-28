7b27ffd8cf7ae801b2285cd7a6eda107ecfc3e6a
shenley <582783985@qq.com>
2026-04-28 09:35:17 +0800

    feat: add RSS feed support

---
+++ app/config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+    title: "Save Point",
+    description: "A simple save point system for your game.",
+    url: "https://toy.shenley.cn",
+    locale: "zh-CN",
+}
---
+++ app/feed.xml/route.ts
@@ -0,0 +1,50 @@
+import { siteConfig } from "../config";
+import { getPosts } from "../lib/posts";
+
+function escapeXml(value: string) {
+  return value
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&apos;");
+}
+
+function toCdata(value: string) {
+  return `<![CDATA[${value.replaceAll("]]>", "]]]]><![CDATA[>")}]]>`;
+}
+
+export async function GET() {
+  const posts = await getPosts();
+
+  const Item = posts
+    .map(
+      (post) => `
+        <item>
+            <title>${escapeXml(post.title)}</title>
+            <link>${siteConfig.url}/posts/${post.slug}</link>
+            <guid>${siteConfig.url}/posts/${post.slug}</guid>
+            <pubDate>${new Date(post.dateTime).toUTCString()}</pubDate>
+            <description>${toCdata(post.excerpt)}</description>
+        </item>
+    `,
+    )
+    .join("");
+
+  const xml = `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+    <channel>
+        <title>${escapeXml(siteConfig.title)}</title>
+        <link>${siteConfig.url}</link>
+        <description>${escapeXml(siteConfig.description)}</description>
+        <language>${escapeXml(siteConfig.locale)}</language>
+        ${Item}
+    </channel>
+</rss>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/rss+xml; charset=utf-8",
+    },
+  });
+}
